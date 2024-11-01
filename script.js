ac5c7e049d95abfc27e43bb018455da9a487fc32
Whoiamvishal <162811409+Whoiamvishal@users.noreply.github.com>
2024-11-01 21:52:24 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+const button = document.getElementById('search-button')
+
+const input = document.getElementById('city-input')
+
+const cityName = document.getElementById('City-Name');
+const cityTime= document.getElementById('City-Time');
+const cityTemp = document.getElementById('City-Temp');
+const cityDate = document.getElementById('City-Date');
+
+
+async function getData(cityName) {
+   const promise = await fetch( `http://api.weatherapi.com/v1/current.json?key=3d2bad89b7b8441587a142254240111&q=${cityName}&aqi=yes`
+
+    )
+    return await promise.json()
+}
+
+button.addEventListener("click", async () => {
+ const value = input.value;
+ const result = await getData(value);
+ 
+cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`
+
+
+    cityTime.innerText = result.location.localtime
+
+cityTemp.innerText = result.current.temp_c
+
+cityCondition.innerText = result.current.condition
+
+})
+
+
+//http://api.weatherapi.com/v1/current.json?key=3d2bad89b7b8441587a142254240111&q=London&aqi=yes
